672c453d42d3a443f56097bc68e3e28320063a73
Jeonghyeok Seo <jeonghyeok.seo@flemingcollege.ca>
2022-01-31 21:56:27 -0500

    add animation

--- story/story.js
+++ story/story.js
@@ -7,6 +7,15 @@ function initMap() {
     new google.maps.Marker({
       position: { lat : 49.35384670547889, lng : -123.10022193015402},
       map,
-      title: "Vancouver",
+      animation: google.maps.Animation.DROP,
     });
+    marker.addListener("click", toggleBounce);
   }
+function toggleBounce() {
+  if (marker.getAnimation()  !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.DROP)
+  }
+  
+}
