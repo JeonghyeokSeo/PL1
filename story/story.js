1f1e7afc6c21c6246283cadf4c2a7336ae23a43d
Jeonghyeok Seo <jeonghyeok.seo@flemingcollege.ca>
2022-01-31 20:22:29 -0500

    map location change, css modified

--- story/story.js
+++ story/story.js
@@ -1,12 +1,11 @@
 function initMap() {
-    const myLatLng = { lat: -25.363, lng: 131.044 };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatLng,
+      zoom: 1,
+      center: { lat : 41.892, lng : -46.012 },
     });
   
     new google.maps.Marker({
-      position: myLatLng,
+      position: "London",
       map,
       title: "First",
     });
